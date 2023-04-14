35224d35571a6a467a4186953df89779d6b9bc82
Curtis Turk <curtis.c.turk@outlook.com>
2023-04-14 12:32:06 +0100

    added some event listeners for inputs

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,21 @@ const numbers = [...Array(10).keys()];
 const specialChars = "!@#$%^&*<>?-_".split("");
 const lettersLower = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 const lettersUpper = lettersLower.map((letter) => letter.toUpperCase());
+
+const passwordLength = document.querySelector("input[type=range]");
+
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+const generatePassword = (e) => {
+  console.log(e);
+};
+
+passwordLength.addEventListener("change", (e) => {
+  generatePassword(e);
+});
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("change", (e) => {
+    console.log("checkbox", e);
+  })
+);
